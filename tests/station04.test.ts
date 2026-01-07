71664310809697d37ac074aa97c5e2ff11868a8e
calloc134 <115209243+calloc134@users.noreply.github.com>
2026-01-07 11:00:25 +0900

    ! マークを3個に変更

--- tests/station04.test.ts
+++ tests/station04.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 import app from '../src/app.js'
 
 describe('Station 04 - API の定義を変更してみよう', () => {
-  it('GET /echo?message=hello&name=techtrain should return {"Message":"hello techtrain!"}', async () => {
+  it('GET /echo?message=hello&name=techtrain should return {"Message":"hello techtrain!!!"}', async () => {
     const res = await app.request('/echo?message=hello&name=techtrain')
     expect(res.status).toBe(200)
 
@@ -10,14 +10,14 @@ describe('Station 04 - API の定義を変更してみよう', () => {
     expect(contentType).toContain('application/json')
 
     const body = await res.json()
-    expect(body).toEqual({ Message: 'hello techtrain!' })
+    expect(body).toEqual({ Message: 'hello techtrain!!!' })
   })
 
-  it('GET /echo?message=Hi&name=World should return {"Message":"Hi World!"}', async () => {
+  it('GET /echo?message=Hi&name=World should return {"Message":"Hi World!!!"}', async () => {
     const res = await app.request('/echo?message=Hi&name=World')
     expect(res.status).toBe(200)
 
     const body = await res.json()
-    expect(body).toEqual({ Message: 'Hi World!' })
+    expect(body).toEqual({ Message: 'Hi World!!!' })
   })
 })
