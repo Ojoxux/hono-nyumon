e1d28e8e3470038028dd308ef96994e46c869745
calloc134 <115209243+calloc134@users.noreply.github.com>
2026-01-07 12:14:20 +0900

    マイグレーション再実行

--- drizzle/0000_steep_magdalene.sql
+++ drizzle/0000_steep_magdalene.sql
@@ -3,18 +3,18 @@ CREATE TABLE "todo_items" (
 	"todo_list_id" integer NOT NULL,
 	"title" varchar(50) NOT NULL,
 	"description" varchar(200),
-	"status_code" integer,
+	"status_code" integer DEFAULT 1 NOT NULL,
 	"due_at" timestamp,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "todo_lists" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"title" varchar(50) NOT NULL,
 	"description" varchar(200),
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "todo_items" ADD CONSTRAINT "todo_items_todo_list_id_todo_lists_id_fk" FOREIGN KEY ("todo_list_id") REFERENCES "public"."todo_lists"("id") ON DELETE restrict ON UPDATE restrict;--> statement-breakpoint
