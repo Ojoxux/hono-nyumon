ce12fd28afb8227a17b4ca92730c14c2809c368f
Jou Okuyama <ojoxux@gmail.com>
2026-05-01 21:54:54 +0900

    get todo list

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import { db } from '../db/client.js'
+import { todoLists } from '../db/schema.js'
 
 const helloRoute = new Hono()
 
@@ -13,6 +15,33 @@ helloRoute.get('/echo', (c) => {
 
 helloRoute.get('/health', (c) => {
   return c.json({ status: `ok` })
+})
+
+// Todoリストを取得するAPI
+helloRoute.get('/lists/:listId', async (c) => {
+  // http://api.example.com/lists/:listId の :listIdの部分をURLから取得する
+  // パスパラメータという
+  const listId = Number(c.req.param('listId'))
+
+  // select * from todoLists where id = **
+  // SQLで書くとこんな感じになる
+  const todoList = await db.query.todoLists.findFirst({
+    where: (todoLists, { eq }) => eq(todoLists.id, listId),
+  })
+
+  // todoListが空なら渡されたlistIdのリストはないということなので、not foundを返す
+  // バリデーションですな
+  if (!todoList) {
+    return c.json({ error: 'Todo list not found' }, 404)
+  }
+
+  return c.json({
+    id: todoList.id,
+    title: todoList.title,
+    description: todoList.description,
+    created_at: todoList.createdAt,
+    updated_at: todoList.updatedAt,
+  })
 })
 
 export default helloRoute
